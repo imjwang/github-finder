ae42487aa1bf04af9fa3d26862db37b1c77d42f8
Jeffrey Wang <ngjaywa@gmail.com>
2023-01-16 15:02:51 -0500

    course finish

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -17,19 +17,13 @@ const githubReducer = (state, action) => {
         users: [],
         loading: false
       }
-    case 'GET_USER':
-      return {
-        ...state,
-        user: action.payload,
-        loading: false
-      }
-
-    case 'GET_REPOS':
-      return {
-        ...state,
-        repos: action.payload,
-        loading: false
-      }
+      case 'GET_USER_AND_REPOS':
+        return {
+          ...state,
+          user: action.payload.user,
+          repos: action.payload.repos,
+          loading: false,
+        }
 
     default:
       return state
